b472fbebbf877f84ddccca478c88d1e1d13b3c3a
Brian Barrington <brianbar@microsoft.com>
2020-12-15 11:03:07 -0600

    accept changes

--- Get Alert Schema Body/TrimAzureAlertSchema.ps1
+++
@@ -1,6 +0,0 @@
-$rawAlertData = '.\rawAlertData.json'
-$sectionToKeep = 'body'
-$json = Get-Content $rawAlertData -raw | ConvertFrom-Json
-$json.$sectionToKeep | 
-ConvertTo-Json -Depth 100 | 
-Out-File .\schemaBody.json
